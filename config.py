8f80ba63f7e2037b8f7818b9dacc0b7a1f2472b8
Artem <artem.tsurukin@icloud.com>
2025-11-30 22:48:56 +0300

    Добавлен requirements.txt, добавлена промежуточная таблица, реализовано добавление пользователя и слов для изучения в БД

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 BOT_TOKEN="7364400756:AAFwQLaey9kUBvzvPA0pdPcvlrDYrhns2_4"
-DATABASE_URL = "postgresql://bot_user:d!gtr43@db:5432/bot_db"
+#DATABASE_URL = "postgresql://bot_user:d!gtr43@db:5432/bot_db"
+DATABASE_URL = "postgresql:///bot_app" # локальная БД
--- database/models.py
+++ database/models.py
@@ -1,22 +1,33 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, BigInteger
 from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
+
+class UserWordAssociation(Base):
+    __tablename__ = 'user_word_association'
+
+    user_id = Column(BigInteger, ForeignKey('users.user_id'), primary_key=True)
+    word_id = Column(Integer, ForeignKey('words.id'), primary_key=True)
+    learned = Column(Boolean, default=False)
+
+    user = relationship("User", back_populates="word_associations")
+    word = relationship("Word", back_populates="user_associations")
+
+
 class User(Base):
     __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    user_id = Column(Integer, nullable=False, unique=True)
+    user_id = Column(BigInteger, primary_key=True, nullable=False, unique=True)
     user_first_name = Column(String(50))
-    learned_words = Column(Integer)
-    unlearned_words = Column(Integer)
+
+    word_associations = relationship("UserWordAssociation", back_populates="user")
 
 
 class Word(Base):
     __tablename__ = "words"
-
     id = Column(Integer, primary_key=True, autoincrement=True)
     word_eng = Column(String(50), nullable=False)
     word_rus = Column(String(50), nullable=False)
+
+    user_associations = relationship("UserWordAssociation", back_populates="word")
