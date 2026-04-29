6438349ca84223e721434425ed28f281f5bd9d51
Artem <artem.tsurukin@icloud.com>
2026-04-29 12:57:40 +0300

    Добавлено объяснение неизвестных слов на OpenAI

--- config.py
+++ config.py
@@ -1,13 +1,16 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv(".env.production")
+#load_dotenv(".env.production")
+load_dotenv(".env.development")
+
 
 class Config:
 
     def __init__(self):
         self.bot_token = os.getenv("BOT_TOKEN")
         self.database_url = os.getenv("DATABASE_URL")
+        self.openai_token = os.getenv("OPENAI_API_KEY")
 
         if not self.bot_token:
             raise ValueError("Токен не установлен")
