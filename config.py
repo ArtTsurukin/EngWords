101200ce8bb054e8ba67a538852390154b3403f3
Artem <artem.tsurukin@icloud.com>
2026-04-29 10:53:29 +0300

    added config.py

--- config.py
+++ config.py
@@ -1,4 +1,18 @@
 import os
+from dotenv import load_dotenv
 
-API_TOKEN = os.getenv("API_TOKEN")
-DB_URL = os.getenv("DB_URL")
+load_dotenv(".env.production")
+
+class Config:
+
+    def __init__(self):
+        self.bot_token = os.getenv("BOT_TOKEN")
+        self.database_url = os.getenv("DATABASE_URL")
+
+        if not self.bot_token:
+            raise ValueError("Токен не установлен")
+        if not self.database_url:
+            raise ValueError("БД не установлена")
+
+
+config_bot = Config()
